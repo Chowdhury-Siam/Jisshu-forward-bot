01f2578d7e6322064c054ade793ef2a779be5a7a
Siam <ssiam4235@gmail.com>
2024-06-16 21:23:13 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/SadKidBGMZ/fwdbot SadKidBGMZ/fwdbot 
+  git clone https://github.com/Chowdhury-Siam/Jisshu-forward-bot Chowdhury-Siam/Jisshu-forward-bot 
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/SadKidBGMZ/fwdbot -b $BRANCH /fwdbot
+  git clone https://github.com/Chowdhury-Siam/Jisshu-forward-bot -b $BRANCH /Jisshu-forward-bot
 fi
-cd SadKidBGMZ/fwdbot 
+cd Chowdhury-Siam/Jisshu-forward-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
